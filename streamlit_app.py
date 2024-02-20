7513d6855bfa53827065bc22f1b5728aa1d6444b
PraveenVajpey <159014464+PraveenVajpey@users.noreply.github.com>
2024-02-20 11:49:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,8 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row=my_cur.fetchall()
+streamlit.header("The Fruit List Contains")
+streamlit.dataframe(my_data_row)
+
